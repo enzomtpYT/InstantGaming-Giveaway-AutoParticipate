b34005dd609b96e7fe05972ea354d4b7b2a73b2a
enzomtp <40535918+enzomtpYT@users.noreply.github.com>
2024-07-28 23:02:07 +0200

    Create no-redirect-participate.user.js

---
+++ no-redirect-participate.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         Instant Gaming Auto Giveaway
+// @description  A script that automatically click on participate buttons on Instant-Gaming.
+// @version      1.2
+// @author       enzomtp
+// @namespace    https://github.com/enzomtpYT/InstantGamingGiveawayList
+// @match        *://www.instant-gaming.com/*/giveaway/*
+// @match        *://github.com/enzomtpYT/*
+// @run-at       document-idle
+// @grant        GM_registerMenuCommand
+// @license      MIT
+// @downloadURL https://github.com/enzomtpYT/InstantGaming-Giveaway-AutoParticipate/raw/main/participate.user.js
+// @updateURL https://github.com/enzomtpYT/InstantGaming-Giveaway-AutoParticipate/raw/main/participate.user.js
+// ==/UserScript==
+
+(function() {
+  "use strict";
+
+  function openInNewTab(url) {
+       window.open(url, '_blank');
+  }
+
+  function participate(){
+    // Get the participate button element.
+    const participate = document.querySelector("button.button.validate");
+    // Click on participate if it exists.
+    if (participate !== null) {
+      participate.click();
+      location.reload();
+    }
+  }
+
+  function socials(){
+    // Get all the socials
+    const socials = document.querySelectorAll("a.button.reward.alerts")
+    // Click on each socials
+    socials.forEach((e) => e.click())
+  }
+
+  function giveawayList(){
+    // Get the giveaway list
+    const giveawayList = document.querySelectorAll("a.giveaway")
+    // Click on the giveaway list
+    giveawayList.forEach((e) => e.click())
+  }
+
+  function openall(){
+      document.querySelectorAll('#user-content-giveaways>a').forEach(a => {openInNewTab(a.href)})
+  }
+
+  // Register the menu command
+  GM_registerMenuCommand("Participate", participate);
+  GM_registerMenuCommand("Socials", socials);
+  GM_registerMenuCommand("Giveaway List", giveawayList);
+  GM_registerMenuCommand("Open ALL Links", openall);
+  participate();
+  socials();
+
+})();
